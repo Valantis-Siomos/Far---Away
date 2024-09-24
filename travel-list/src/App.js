1d66408bfcebf1b7c3e9a6fd31a5a9158a914767
Chrysovalantis Siomos <SIOPIS2008@HOTMAIL.COM>
2024-09-24 15:43:47 +0300

    build-components

--- travel-list/src/App.js
+++ travel-list/src/App.js
@@ -1,43 +1,58 @@
-
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: false },
+  { id: 2, description: "Socks", quantity: 12, packed: false },
+];
 
 export default function App() {
   return (
-  <div className="app">
-    <Logo />
-    <Form />
-    <PackingList />
-    <Stats />
-  </div>
-  )
+    <div className="app">
+      <Logo />
+      <Form />
+      <PackingList />
+      <Stats />
+    </div>
+  );
 }
 
 function Logo() {
-  return (
-  <h1> 🌴 Far Away 👜 </h1>
-  )
+  return <h1> 🌴 Far Away 👜 </h1>;
 }
 
 function Form() {
   return (
-  <div className="add-form">
-    <h3>What do you need for your trip?</h3>
-  </div>
-  )
+    <div className="add-form">
+      <h3>What do you need for your trip?</h3>
+    </div>
+  );
 }
 
 function PackingList() {
   return (
-   <div className="list">List</div>
-  )
+    <div className="list">
+      {/* css line 99 comment because appear scroll bars */}
+      <ul>
+        {initialItems.map((item) => (
+          <Item item={item} />
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+function Item({ item }) {
+  return (
+    <li>
+      <span>
+        {item.quantity} {item.description}
+      </span>
+    </li>
+  );
 }
 
 function Stats() {
   return (
-  <footer className="stats">
-    <em>
-    You have x items on your list, and you are already packed X (X%)
-    </em>
-  </footer>
-  )
+    <footer className="stats">
+      <em>You have x items on your list, and you are already packed X (X%)</em>
+    </footer>
+  );
 }
-
